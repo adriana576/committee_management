40ec9eaf5c5d8b9b80625b72dae75c316177730f
Adriana576 <nuralesya439@gmail.com>
2026-06-23 14:44:42 +0800

    Update

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2');
 
 // Create connection to Railway MySQL
+const mysql = require('mysql2');
+
 const connection = mysql.createConnection({
-  host: process.env.RAILWAY_PRIVATE_DOMAIN, // Railway internal host
-  user: process.env.DB_USER,                // DB user from Railway
-  password: process.env.DB_PASSWORD,        // DB password from Railway
-  database: process.env.DB_NAME,            // DB name from Railway
-  port: process.env.DB_PORT || 3306         // Usually 3306
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
+
+module.exports = connection;
 
 // Connect
 connection.connect(err => {
