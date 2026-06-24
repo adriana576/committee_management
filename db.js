2a4525ead157b91aa99baffbe44e8972b1ced1ab
Adriana576 <nuralesya439@gmail.com>
2026-06-24 15:45:25 +0800

    update

--- db.js
+++ db.js
@@ -1,16 +1,5 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-console.log("MYSQL HOST:", process.env.MYSQL_HOST);
-console.log("MYSQL USER:", process.env.MYSQL_USER);
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-  port: 3306,
-  ssl: { rejectUnauthorized: false }
-});
-
-module.exports = pool.promise();
+module.exports = pool;
