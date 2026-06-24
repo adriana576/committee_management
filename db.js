12fc2f62be7f959e74f3cb924c7591fbd2de35a5
Adriana576 <nuralesya439@gmail.com>
2026-06-24 23:08:53 +0800

    update

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST, // use private host
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT, // usually 3306
+  host: process.env.MYSQLHOST, // use private host
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT, // usually 3306
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
