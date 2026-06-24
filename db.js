3417ca24b01626483744484365031c67ed81b51f
Adriana576 <nuralesya439@gmail.com>
2026-06-24 15:15:09 +0800

    update

--- db.js
+++ db.js
@@ -8,7 +8,9 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
+  port: process.env.MYSQL_PORT,
+  port: 3306,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool.promise();
