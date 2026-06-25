0f102642c06782f0d1d0e0afaf816a6bc34bc9c2
Adriana576 <nuralesya439@gmail.com>
2026-06-25 23:45:54 +0800

    update

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 const mysql = require('mysql2');
 
+const mysql = require('mysql2');
+
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: Number(process.env.MYSQL_PORT),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  ssl: {
-  rejectUnauthorized: false
-  },
-  enableKeepAlive: true,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -17,9 +15,9 @@ const pool = mysql.createPool({
 
 pool.getConnection((err, connection) => {
   if (err) {
-    console.error('Database connection failed:', err);
+    console.error('Database connection failed:', err.message);
   } else {
-    console.log('Connected to Aiven MySQL');
+    console.log('Connected to FreeDB MySQL');
     connection.release();
   }
 });
