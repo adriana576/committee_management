58e66311bfba286d7525e1a28e15abbc178f83e1
Adriana576 <nuralesya439@gmail.com>
2026-07-23 19:24:40 +0800

    Updated

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'srv2133.hstgr.io',
+  family: 4,
   user: 'u125391022_CommitteeMng',
   password: '@syA4789463',
   database: 'u125391022_CMS',
