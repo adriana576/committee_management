37e56909b6dbff83b38607439724def9c51c6713
Adriana576 <nuralesya439@gmail.com>
2026-07-23 19:07:03 +0800

    Updated

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Make sure dotenv is installed
 
 const db = mysql.createConnection({
-    host: 'srv2133.hstgr.io',
-    user: 'u125391022_CommitteeMng',
-    password: '@syA4789463',
-    database: 'u125391022_CMS',
-    port: 3306
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 db.connect(err => {
     if(err){
-        console.log(err);
-    }else{
+        console.error("Error connecting to Hostinger MySQL:", err);
+    } else {
         console.log("Connected to Hostinger MySQL");
     }
 });
